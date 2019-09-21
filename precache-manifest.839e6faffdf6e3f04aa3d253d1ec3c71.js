b84a7097343bcb8863fb8720b63820400f10584e
Your Name <you@example.com>
2019-09-21 23:41:22 +0530

    Updates

---
+++ precache-manifest.839e6faffdf6e3f04aa3d253d1ec3c71.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ffe0ceb057be532e17463e76c2c5d0d",
+    "url": "/calvinroshan/production.git/index.html"
+  },
+  {
+    "revision": "50bb18fa63bf36c57aa2",
+    "url": "/calvinroshan/production.git/static/css/2.3b02b1b6.chunk.css"
+  },
+  {
+    "revision": "913da0c674f9fc935a65",
+    "url": "/calvinroshan/production.git/static/css/main.c1681fac.chunk.css"
+  },
+  {
+    "revision": "50bb18fa63bf36c57aa2",
+    "url": "/calvinroshan/production.git/static/js/2.ec5e214b.chunk.js"
+  },
+  {
+    "revision": "913da0c674f9fc935a65",
+    "url": "/calvinroshan/production.git/static/js/main.61928ce7.chunk.js"
+  },
+  {
+    "revision": "a15d6ec7ea47140093dd",
+    "url": "/calvinroshan/production.git/static/js/runtime~main.40eaa92d.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/calvinroshan/production.git/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/calvinroshan/production.git/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/calvinroshan/production.git/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/calvinroshan/production.git/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/calvinroshan/production.git/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/calvinroshan/production.git/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
